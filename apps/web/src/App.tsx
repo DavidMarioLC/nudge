45c1f03850fe5cfc73abf6554220744236fa5c0c
DavidMarioLC <davidmariolc.dev@gmail.com>
2026-05-07 14:37:47 -0500

    feat: add auth and login loaders

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,16 +1,71 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import type { LoaderFunctionArgs } from "react-router";
+import { createBrowserRouter, RouterProvider, redirect } from "react-router";
+import { AppLayout } from "@/layouts/AppLayout";
+import { getSession } from "@/lib/auth-client";
+import DashboardPage from "@/pages/DashboardPage";
+import NotFoundPage from "@/pages/NotFoundPage";
+import ProjectsPage from "@/pages/ProjectsPage";
 import SignInPage from "@/pages/SignInPage";
 import SignUpPage from "@/pages/SignUpPage";
 
+async function authLoader({ request }: LoaderFunctionArgs) {
+	const { data: session } = await getSession();
+
+	if (!session) {
+		// Guardamos la URL destino + los query params
+		const url = new URL(request.url);
+		const next = url.pathname + url.search;
+
+		throw redirect(`/?next=${encodeURIComponent(next)}`);
+	}
+
+	return { user: session.user };
+}
+
+async function loginLoader(_: LoaderFunctionArgs) {
+	const { data: session } = await getSession();
+
+	if (session) {
+		throw redirect("/dashboard");
+	}
+
+	return null;
+}
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		index: true,
+		Component: SignInPage,
+		loader: loginLoader,
+	},
+	{
+		path: "/signup",
+		Component: SignUpPage,
+		loader: loginLoader,
+	},
+	{
+		element: <AppLayout />,
+		loader: authLoader,
+		children: [
+			{
+				path: "/dashboard",
+				Component: DashboardPage,
+			},
+			{
+				path: "/projects",
+				Component: ProjectsPage,
+			},
+		],
+	},
+	{
+		path: "*",
+		Component: NotFoundPage,
+	},
+]);
+
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route index path="/" element={<SignInPage />} />
-				<Route path="/signup" element={<SignUpPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;

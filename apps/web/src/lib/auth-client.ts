7e46e962941c2aedf30b11f41490ff2ea63d6981
DavidMarioLC <davidmariolc.dev@gmail.com>
2026-05-07 14:40:20 -0500

    feat: add better-auth client

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,6 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 
-export const { signIn, signUp, useSession } = createAuthClient({
-	/** The base URL of the server (optional if you're using the same domain) */
-	baseURL: "http://localhost:3005",
-});
+export const { signIn, signUp, signOut, useSession, getSession } =
+	createAuthClient({
+		baseURL: "http://localhost:3005",
+		fetchOptions: {
+			credentials: "include",
+		},
+	});
